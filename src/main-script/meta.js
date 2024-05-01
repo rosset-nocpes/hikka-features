83c7d0a198c05c78e133b92d92724bee199321a6
~rosset-nocpes | Andrii <coderpy4@proton.me>
2024-05-02 00:26:28 +0300

    add homepageURL

--- src/main-script/meta.js
+++ src/main-script/meta.js
@@ -5,6 +5,7 @@
 // @match       https://hikka.io/*
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
+// @homepageURL https://github.com/rosset-nocpes/hikka-features
 // @downloadURL https://rosset-nocpes.github.io/hikka-features/hikka-features.user.js
 // @grant       GM_addStyle
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom
