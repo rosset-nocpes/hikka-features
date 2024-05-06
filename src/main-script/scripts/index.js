7ffdfad1ef8ae04aaa0bccf7d9355c0036781cd8
Andrii Blacksmith <coderpy4@proton.me>
2024-05-07 00:39:40 +0300

    refactor: make main function as js module

--- src/main-script/scripts/index.js
+++ src/main-script/scripts/index.js
@@ -6,3 +6,4 @@ export {
 } from './amanogawa-button.js';
 export { default as aniButtons } from './ani-buttons.js';
 export { default as aniBackground } from './ani-background.js';
+export { default as Main } from './main.js';
