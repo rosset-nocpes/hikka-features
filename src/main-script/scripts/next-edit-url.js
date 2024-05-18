80e8f25232aabe7d6729cd61c9b3fb91a7df12f0
Andrii Blacksmith <coderpy4@proton.me>
2024-05-18 19:22:23 +0300

    fix(next-edit-url): cycle edits

--- src/main-script/scripts/next-edit-url.js
+++ src/main-script/scripts/next-edit-url.js
@@ -15,9 +15,27 @@ export default async function NextEditURL(edit_id) {
   ).json();
 
   if (pending_edits['pagination']['total'] != 0) {
-    for (const [i, elem] of pending_edits['list'].entries()) {
-      if (elem['edit_id'] == edit_id) {
-        return `https://hikka.io/edit/${pending_edits['list'][i + 1]['edit_id']}`;
+    for (let i = 1; i <= pending_edits['pagination']['pages']; i++) {
+      const list = (
+        await (
+          await fetch(`https://api.hikka.io/edit/list?page=${i}&size=100`, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              status: 'pending',
+              slug: '',
+            }),
+          })
+        ).json()
+      )['list'];
+
+      for (const [j, elem] of list.entries()) {
+        if (elem['edit_id'] == edit_id) {
+          return `https://hikka.io/edit/${list[j + 1 > list.length - 1 ? 0 : j + 1]['edit_id']}`;
+        }
       }
     }
   }
