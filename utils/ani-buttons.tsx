99a92d072102bafe47c16d4a1be14b1dc34dcbe8
Andrii Blacksmith <coderpy4@proton.me>
2024-05-30 23:28:50 +0300

    style(ani-buttons): full redesign & add amanogawa url

--- utils/ani-buttons.tsx
+++ utils/ani-buttons.tsx
@@ -4,9 +4,11 @@
 // @author      ~rosset-nocpes
 // @description In future will be based on https://greasyfork.org/en/scripts/398046-anime-website-custom-buttons-plus
 
-import getAmanogawaId, { amanogawaURL } from "./amanogawa-button";
+import { For, render } from "solid-js/web";
+import getAmanogawaURL from "./amanogawa-button";
+import HikkaFLogoSmall from "@/public/hikka-features-small.svg";
 
-export default function aniButtons(data) {
+export default async function aniButtons(data, location) {
   const content_type = data.data_type;
 
   const contentTypeMap = {
@@ -33,58 +35,83 @@ export default function aniButtons(data) {
       ? data.external.find((obj) => obj.url?.includes(hosts[website]))?.url
       : null;
 
-  const urls = {
-    mal: `https://myanimelist.net/${
-      contentTypeMap[content_type]?.mal || content_type
-    }.php?q=${title}`,
-    al: `https://anilist.co/search/${
-      contentTypeMap[content_type]?.al || content_type
-    }?search=${title}&sort=SEARCH_MATCH`,
-    ad:
-      getUrl("ad") ??
-      `https://anidb.net/${
-        contentTypeMap[content_type]?.ad || content_type
-      }/?adb.search=${
-        ["person", "character"].includes(content_type) &&
-        title.split(" ").length == 2
-          ? `${title.split(" ")[1]} ${title.split(" ")[0]}`
-          : title
-      }&do.search=1`,
-    ann: getUrl("ann") ?? `https://www.animenewsnetwork.com/search?q=${title}`,
-    wiki:
-      content_type !== "character"
-        ? getUrl("wiki") ??
-          `https://en.wikipedia.org/w/index.php?search=${title}`
-        : null,
-    agawa: isAnime ? amanogawaURL(getAmanogawaId(data)) : null,
-  };
-
-  console.log(urls.agawa);
-
   const searchUrls = [
-    { host: hosts.mal, url: urls.mal },
-    { host: hosts.al, url: urls.al },
-    { host: hosts.ad, url: urls.ad },
-    { host: hosts.ann, url: urls.ann },
-    { host: hosts.wiki, url: urls.wiki },
-    { host: hosts.agawa, url: urls.agawa },
+    {
+      title: "MyAnimeList",
+      host: hosts.mal,
+      url: `https://myanimelist.net/${
+        contentTypeMap[content_type]?.mal || content_type
+      }.php?q=${title}`,
+    },
+    {
+      title: "AniList",
+      host: hosts.al,
+      url: `https://anilist.co/search/${
+        contentTypeMap[content_type]?.al || content_type
+      }?search=${title}&sort=SEARCH_MATCH`,
+    },
+    {
+      title: "AniDB",
+      host: hosts.ad,
+      url:
+        getUrl("ad") ??
+        `https://anidb.net/${
+          contentTypeMap[content_type]?.ad || content_type
+        }/?adb.search=${
+          ["person", "character"].includes(content_type) &&
+          title.split(" ").length === 2
+            ? `${title.split(" ")[1]} ${title.split(" ")[0]}`
+            : title
+        }&do.search=1`,
+    },
+    {
+      title: "ANN",
+      host: hosts.ann,
+      url:
+        getUrl("ann") ?? `https://www.animenewsnetwork.com/search?q=${title}`,
+    },
+    {
+      title: "Wikipedia",
+      host: hosts.wiki,
+      url:
+        content_type !== "character"
+          ? getUrl("wiki") ??
+            `https://en.wikipedia.org/w/index.php?search=${title}`
+          : null,
+    },
+    {
+      title: "Amanogawa",
+      host: hosts.agawa,
+      url: isAnime ? await getAmanogawaURL(data) : null,
+    },
   ];
 
-  let ani_buttons = [];
-
-  searchUrls.forEach((element) => {
-    if (element.url == null) {
-      return;
-    }
-    ani_buttons.push(
-      <a style="display: flex;" href={element.url} target="_blank">
-        <img
-          style="width:16px;height:16px;margin-right:2px;"
-          src={`https://www.google.com/s2/favicons?domain=${element.host}`}
-        />
-      </a>
-    );
-  });
-
-  return ani_buttons;
+  render(
+    () => (
+      <div class="ani-buttons">
+        <h3 class="scroll-m-20 font-display text-xl font-bold tracking-normal">
+          Інші джерела
+          <img src={HikkaFLogoSmall} style="width: 21px; height: 20px" />
+        </h3>
+        <div>
+          <For each={searchUrls}>
+            {(elem) => (
+              <a
+                class={elem.url ?? "link-disabled"}
+                href={elem.url}
+                target="_blank"
+              >
+                <img
+                  style="width:16px;height:16px;margin-right:2px;"
+                  src={`https://www.google.com/s2/favicons?domain=${elem.host}`}
+                />
+                {elem.title}
+              </a>
+            )}
+          </For>
+        </div>
+      </div>
+    ),
+    location
+  );
 }
