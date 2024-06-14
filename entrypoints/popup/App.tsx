1efc83c19acda23ad2d0b8e91dd8e597d800f1c0
Andrii Blacksmith <coderpy4@proton.me>
2024-06-14 18:37:18 +0300

    style(settings): set root bg to black

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -25,82 +25,85 @@ const [showWatchButton, toggleWatchButton] = createSignal(
 
 function App() {
   return (
-    <div class="dark bg-black text-white p-[30px] w-[400px] flex flex-col gap-7 font-inter font-semibold">
-      <h3 class="flex justify-between items-center text-lg font-bold tracking-normal">
-        Налаштування
-      </h3>
-      <div class="flex flex-col gap-5">
-        <Switch
-          checked={showWatchButton()}
-          onClick={() => {
-            !showWatchButton()
-              ? watchButtonState.setValue(true)
-              : watchButtonState.setValue(false);
-            toggleWatchButton(!showWatchButton());
-          }}
-          class="flex items-center justify-between"
-        >
-          <div class="flex flex-col gap-1 mr-10">
-            <SwitchLabel>Перегляд</SwitchLabel>
-            <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
-              Кнопка для відображення плеєру
-            </SwitchDescription>
-          </div>
-          <SwitchControl>
-            <SwitchThumb />
-          </SwitchControl>
-        </Switch>
-        <Switch
-          checked={showAniButtons()}
-          onClick={() => {
-            !showAniButtons()
-              ? aniButtonsState.setValue(true)
-              : aniButtonsState.setValue(false);
-            toggleAniButtons(!showAniButtons());
-          }}
-          class="flex items-center justify-between"
-        >
-          <div class="flex flex-col gap-1 mr-10">
-            <SwitchLabel>Інші джерела</SwitchLabel>
-            <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
-              Додаткові посилання на сторінках тайтлів
-            </SwitchDescription>
-          </div>
-          <SwitchControl>
-            <SwitchThumb />
-          </SwitchControl>
-        </Switch>
-        <Switch
-          checked={showAniBackground()}
-          onClick={() => {
-            !showAniBackground()
-              ? aniBackState.setValue(true)
-              : aniBackState.setValue(false);
-            toggleAniBackground(!showAniBackground());
-          }}
-          class="flex items-center justify-between"
-        >
-          <div class="flex flex-col gap-1 mr-10">
-            <SwitchLabel>Обкладинки</SwitchLabel>
-            <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
-              Покращене оформлення на сторінках тайтлів та персонажів
-            </SwitchDescription>
-          </div>
-          <SwitchControl>
-            <SwitchThumb />
-          </SwitchControl>
-        </Switch>
+    <>
+      <style>{`:root { background-color: black; }`}</style>
+      <div class="dark bg-black text-white p-[30px] w-[400px] flex flex-col gap-7 font-inter font-semibold rounded-none">
+        <h3 class="flex justify-between items-center text-lg font-bold tracking-normal">
+          Налаштування
+        </h3>
+        <div class="flex flex-col gap-5">
+          <Switch
+            checked={showWatchButton()}
+            onClick={() => {
+              !showWatchButton()
+                ? watchButtonState.setValue(true)
+                : watchButtonState.setValue(false);
+              toggleWatchButton(!showWatchButton());
+            }}
+            class="flex items-center justify-between"
+          >
+            <div class="flex flex-col gap-1 mr-10">
+              <SwitchLabel>Перегляд</SwitchLabel>
+              <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
+                Кнопка для відображення плеєру
+              </SwitchDescription>
+            </div>
+            <SwitchControl>
+              <SwitchThumb />
+            </SwitchControl>
+          </Switch>
+          <Switch
+            checked={showAniButtons()}
+            onClick={() => {
+              !showAniButtons()
+                ? aniButtonsState.setValue(true)
+                : aniButtonsState.setValue(false);
+              toggleAniButtons(!showAniButtons());
+            }}
+            class="flex items-center justify-between"
+          >
+            <div class="flex flex-col gap-1 mr-10">
+              <SwitchLabel>Інші джерела</SwitchLabel>
+              <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
+                Додаткові посилання на сторінках тайтлів
+              </SwitchDescription>
+            </div>
+            <SwitchControl>
+              <SwitchThumb />
+            </SwitchControl>
+          </Switch>
+          <Switch
+            checked={showAniBackground()}
+            onClick={() => {
+              !showAniBackground()
+                ? aniBackState.setValue(true)
+                : aniBackState.setValue(false);
+              toggleAniBackground(!showAniBackground());
+            }}
+            class="flex items-center justify-between"
+          >
+            <div class="flex flex-col gap-1 mr-10">
+              <SwitchLabel>Обкладинки</SwitchLabel>
+              <SwitchDescription class="text-xs font-medium text-[#A1A1A1]">
+                Покращене оформлення на сторінках тайтлів та персонажів
+              </SwitchDescription>
+            </div>
+            <SwitchControl>
+              <SwitchThumb />
+            </SwitchControl>
+          </Switch>
+        </div>
+        <div class="flex items-center justify-center text-xs text-[#5C5C5C] gap-1">
+          <a
+            href="https://github.com/rosset-nocpes/hikka-features"
+            class="font-bold"
+          >
+            GitHub
+          </a>
+          <a>•</a>v{version}
+        </div>
       </div>
-      <div class="flex items-center justify-center text-xs text-[#5C5C5C] gap-1">
-        <a
-          href="https://github.com/rosset-nocpes/hikka-features"
-          class="font-bold"
-        >
-          GitHub
-        </a>
-        <a>•</a>v{version}
-      </div>
-    </div>
+    </>
   );
 }
 
