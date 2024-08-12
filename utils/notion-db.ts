6395495a24f836767a2a43172e1ce8281210dbdb
Andrii Blacksmith <coderpy4@proton.me>
2024-08-12 11:07:55 +0300

    rename notion enpoint

--- utils/notion-db.ts
+++ utils/notion-db.ts
@@ -1,7 +1,7 @@
-export default async function Teams(slug: string) {
+export default async function NotionFetch(slug: string) {
   return await (
     await fetch(
-      `${BACKEND_BRANCHES[await backendBranch.getValue()]}/fandub/${slug}`
+      `${BACKEND_BRANCHES[await backendBranch.getValue()]}/notion/${slug}`
     )
   ).json();
 }
