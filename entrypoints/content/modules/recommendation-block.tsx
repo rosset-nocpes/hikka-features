3c866ac672e6c5a0e57a33005b5425b33578cff1
Lorg0n <lorgon.kv@gmail.com>
2025-01-25 01:21:26 +0300

    fix: change outdate site to MAL

--- entrypoints/content/modules/recommendation-block.tsx
+++ entrypoints/content/modules/recommendation-block.tsx
@@ -4,9 +4,9 @@ import { Transition } from "solid-transition-group";
 import { Button } from "@/components/ui/button";
 import MaterialSymbolsArrowRightAltRounded from "~icons/material-symbols/arrow-right-alt-rounded";
 
-async function fetchDetailedData(anibrainItem: any) {
-  const malId = anibrainItem.myanimelistId;
-  const response = await fetch(`https://hikka.io/api/integrations/mal/anime/${malId}`);
+async function fetchDetailedData(content: any) {
+  const malId = content.entry.mal_id;
+  const response = await fetch(`https://api.hikka.io/integrations/mal/anime/${malId}`);
   if (!response.ok) {
     throw new Error(`API request failed with status code ${response.status}`);
   }
@@ -31,36 +31,28 @@ export default async function recommendationBlock(
   
   const [loading, setLoading] = createSignal(true);
   const title = anime_data.title_ja || anime_data.title_en || anime_data.title_original || anime_data.name_en;
+  const mal_id = anime_data.mal_id;
 
-  async function fetchRecommendations(title_en: string) {
+  var maxSingleVotes = 0;
+
+  async function fetchRecommendations() {
     try {
-      const searchResponse = await fetch(
-        `https://corsproxy.io/?https://anibrain.ai/api/-/recommender/autosuggest?searchValue=${title_en}&mediaType=ANIME&adult=false`
-      );
-      const searchData = await searchResponse.json();
-
-      const mediaId = searchData?.data?.[0]?.id;
-
-      if (!mediaId) {
-        console.warn("Media ID not found, skipping recommendation request.");
-        setLoading(false);
-        return;
-      }
       const recommendationResponse = await fetch(
-        `https://corsproxy.io/?https://anibrain.ai/api/-/recommender/recs/anime?filterCountry=[]&filterFormat=["movie","ona","tv"]&filterGenre={}&filterTag={"max":{},"min":{}}&filterRelease=[1917,2024]&filterScore=0&algorithmWeights={"genre":0.3,"setting":0.15,"synopsis":0.4,"theme":0.2}&mediaId=${mediaId}&mediaType=ANIME&adult=false&page=1`
+        `https://api.jikan.moe/v4/anime/${mal_id}/recommendations`
       );
       const recommendationData = await recommendationResponse.json();
-
       var result: Promise<any>[] = [];
       for (const element of recommendationData.data.slice(0, 4)) {
         try {
           const hikkaData = await fetchDetailedData(element);
-          result.push({...hikkaData, anibrain: {...element}});
+          result.push({...hikkaData, mal: {...element}});
+          if (element.votes > maxSingleVotes) { maxSingleVotes = element.votes}
           await new Promise((resolve) => setTimeout(resolve, 250));
         } catch (error) {
           await new Promise((resolve) => setTimeout(resolve, 700)); 
           const hikkaData = await fetchDetailedData(element); 
-          result.push({...hikkaData, anibrain: {...element}});
+          if (element.votes > maxSingleVotes) { maxSingleVotes = element.votes}
+          result.push({...hikkaData, mal: {...element}});
         }
       }
       setRecommendations(result || []);
@@ -72,7 +64,7 @@ export default async function recommendationBlock(
   }
 
   onMount(() => {
-    fetchRecommendations(title);
+    fetchRecommendations();
   });
   
   render(
@@ -87,7 +79,7 @@ export default async function recommendationBlock(
               <Button size="icon-sm" variant="outline" disabled={loading() || recommendations().length == 0}>
                 {
                 <a
-                  href={`https://anibrain.ai/recommender/anime?search_id=${recommendations()[0]?.anibrain?.edgeId?.split("_")[0]}&search_title`}
+                  href={`https://myanimelist.net/anime/${mal_id}`}
                   target="_blank"
                 >
                   <MaterialSymbolsArrowRightAltRounded class="text-lg" />
@@ -117,24 +109,24 @@ export default async function recommendationBlock(
                         >
                           <img
                             class="object-cover w-full h-full absolute inset-0 bg-secondary/30"
-                            src={item.poster}
+                            src={item.image}
                             alt={item.title_ua || item.title_en || item.title_ja}
                             loading="lazy"
                           />
                           <p
                             class={`absolute top-2 left-2 px-2 py-1 rounded-md bg-secondary/80 text-sm ${
-                              item.anibrain.score > 90
+                              Math.round(item?.mal.votes / maxSingleVotes * 100) > 90
                                 ? "text-success"
-                                : item.anibrain.score > 80
+                                : Math.round(item?.mal.votes / maxSingleVotes * 100) > 50
                                 ? "text-warning"
                                 : "text-destructive"
                             }`}
                           >
-                            {item.anibrain.score}
+                            {Math.round(item?.mal.votes / maxSingleVotes * 100)}
                           </p>
                         </div>
                         <label class="text-sm font-medium leading-5 line-clamp-2">
-                          {item.title_ua || item.title_en || item.title_ja}
+                          {item?.title_ua || item?.title_en || item?.title_ja}
                         </label>
                       </div>
                     </a>
@@ -146,7 +138,7 @@ export default async function recommendationBlock(
     ),
     recommendationBlockLocation ||
       document.querySelector(
-        "body > main > div > div.flex.flex-col.gap-12 > div.grid.grid-cols-1.gap-12 > div.relative.order-2.flex.flex-col.gap-12"
+        "body > div > main > div > div.flex.flex-col.gap-12 > div.grid.grid-cols-1.gap-12 > div.relative.order-2.flex.flex-col.gap-12"
       )!
   );
 }  
