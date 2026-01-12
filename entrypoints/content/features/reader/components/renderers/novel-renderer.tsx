dde9aa3b8f9d31c45bd6327bc2ed811855072000
Andrii Blacksmith <coderpy4@proton.me>
2026-01-12 02:59:19 +0200

    feat(novel-reader): add reading scroll progress indicator

--- entrypoints/content/features/reader/components/renderers/novel-renderer.tsx
+++ entrypoints/content/features/reader/components/renderers/novel-renderer.tsx
@@ -2,7 +2,13 @@ import parse, {
   domToReact,
   type HTMLReactParserOptions,
 } from 'html-react-parser';
-import { AnimatePresence, motion } from 'motion/react';
+import {
+  AnimatePresence,
+  type HTMLMotionProps,
+  motion,
+  useScroll,
+} from 'motion/react';
+import { forwardRef, useRef, useState } from 'react';
 import {
   HoverCard,
   HoverCardContent,
@@ -12,7 +18,6 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { Skeleton } from '@/components/ui/skeleton';
 import useReadChapterData from '../../hooks/use-read-chapter-data';
 import { useReader } from '../../hooks/use-reader';
-import { ReaderType } from '../../reader.enums';
 
 const layoutAnimation = {
   initial: { opacity: 0 },
@@ -21,87 +26,121 @@ const layoutAnimation = {
   transition: { duration: 0.2 },
 };
 
-// todo: add progress bar for how far have been scrolled
 const NovelRenderer = () => {
   const { currentChapter, settings } = useReader();
   const { data: chapterPage, isLoading } = useReadChapterData();
 
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ container: scrollRef });
+
   if (Array.isArray(chapterPage)) return;
 
-  return (
-    <div
-      className={cn(
-        'flex w-full justify-center overflow-y-auto',
-        // settings.type === ReaderType.Novel && settings.fontFamily,
-      )}
-    >
-      <ScrollArea className="w-full max-w-4xl">
-        <div className="mt-48 mb-10 flex flex-col">
-          <p className="font-bold text-muted-foreground text-sm uppercase tracking-widest">
-            Том {currentChapter?.volume} Розділ {currentChapter?.chapter}
-          </p>
-          <h1 className="scroll-m-20 whitespace-nowrap text-balance font-extrabold text-4xl tracking-tight">
-            {currentChapter?.title}
-          </h1>
-        </div>
+  const handleAnimationStart = () => {
+    setAnimationComplete(false);
+  };
 
-        <AnimatePresence mode="wait">
-          {chapterPage && !isLoading && (
-            <PageParser htmlContent={parseNovelPage(chapterPage) || ''} />
-          )}
-          {isLoading && (
-            <motion.div className="flex flex-col gap-4" {...layoutAnimation}>
-              {Array.from({ length: 15 }).map((_, index) => (
-                <div key={index}>
-                  <Skeleton className="h-4 w-full" />
-                </div>
-              ))}
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </ScrollArea>
-    </div>
-  );
-};
-
-const PageParser = ({ htmlContent }: { htmlContent: string }) => {
-  const { container } = useReader();
-
-  const options: HTMLReactParserOptions = {
-    replace: (domNode: any) => {
-      if (domNode.attribs && domNode.attribs.class === 'hover-card-trigger') {
-        const term = domNode.attribs['data-term'];
-        const definition = domNode.attribs['data-definition'];
-
-        return (
-          <HoverCard openDelay={200} closeDelay={100}>
-            <HoverCardTrigger asChild>
-              <span className="cursor-help text-primary underline decoration-dotted underline-offset-4">
-                {domToReact(domNode.children)}
-              </span>
-            </HoverCardTrigger>
-            <HoverCardContent className="w-80" container={container}>
-              <div className="flex flex-col gap-2">
-                <h4 className="font-semibold text-sm">{term}</h4>
-                <p className="text-muted-foreground text-sm leading-relaxed">
-                  {definition}
-                </p>
-              </div>
-            </HoverCardContent>
-          </HoverCard>
-        );
-      }
-    },
+  const handleAnimationComplete = () => {
+    setAnimationComplete(true);
   };
 
   return (
-    <motion.div
-      className="[&_img]:rounded-md [&_img]:border [&_p:not(:first-child)]:mt-6 [&_p]:leading-7 [&_span]:inline"
-      {...layoutAnimation}
-    >
-      {parse(htmlContent, options)}
-    </motion.div>
+    <>
+      <motion.div
+        id="scroll-indicator"
+        className="absolute top-0 right-0 left-0 z-10 h-1 origin-left bg-primary"
+        style={{
+          scaleX: animationComplete && !isLoading ? scrollYProgress : 0,
+        }}
+      />
+
+      <div
+        className={cn(
+          'flex w-full justify-center overflow-y-auto px-3',
+          // settings.type === ReaderType.Novel && settings.fontFamily,
+        )}
+      >
+        <ScrollArea ref={scrollRef} className="w-full max-w-4xl">
+          <div className="mt-48 mb-10 flex flex-col">
+            <p className="font-bold text-muted-foreground text-sm uppercase tracking-widest">
+              Том {currentChapter?.volume} Розділ {currentChapter?.chapter}
+            </p>
+            <h1 className="scroll-m-20 whitespace-nowrap text-balance font-extrabold text-4xl tracking-tight">
+              {currentChapter?.title}
+            </h1>
+          </div>
+
+          <AnimatePresence mode="wait">
+            {chapterPage && !isLoading && (
+              <PageParser
+                htmlContent={parseNovelPage(chapterPage) || ''}
+                onAnimationStart={handleAnimationStart}
+                onAnimationComplete={handleAnimationComplete}
+              />
+            )}
+            {isLoading && (
+              <motion.div className="flex flex-col gap-4" {...layoutAnimation}>
+                {Array.from({ length: 15 }).map((_, index) => (
+                  <div key={index}>
+                    <Skeleton className="h-4 w-full" />
+                  </div>
+                ))}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </ScrollArea>
+      </div>
+    </>
   );
 };
+
+interface PageParserProps extends HTMLMotionProps<'div'> {
+  htmlContent: string;
+}
+
+const PageParser = forwardRef<HTMLDivElement, PageParserProps>(
+  ({ htmlContent, ...props }, ref) => {
+    const { container } = useReader();
+
+    const options: HTMLReactParserOptions = {
+      replace: (domNode: any) => {
+        if (domNode.attribs && domNode.attribs.class === 'hover-card-trigger') {
+          const term = domNode.attribs['data-term'];
+          const definition = domNode.attribs['data-definition'];
+
+          return (
+            <HoverCard openDelay={200} closeDelay={100}>
+              <HoverCardTrigger asChild>
+                <span className="cursor-help text-primary underline decoration-dotted underline-offset-4">
+                  {domToReact(domNode.children)}
+                </span>
+              </HoverCardTrigger>
+              <HoverCardContent className="w-80" container={container}>
+                <div className="flex flex-col gap-2">
+                  <h4 className="font-semibold text-sm">{term}</h4>
+                  <p className="text-muted-foreground text-sm leading-relaxed">
+                    {definition}
+                  </p>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
+          );
+        }
+      },
+    };
+
+    return (
+      <motion.div
+        className="[&_img]:rounded-md [&_img]:border [&_p:not(:first-child)]:mt-6 [&_p]:leading-7 [&_span]:inline"
+        ref={ref}
+        {...layoutAnimation}
+        {...props}
+      >
+        {parse(htmlContent, options)}
+      </motion.div>
+    );
+  },
+);
 
 export default NovelRenderer;
