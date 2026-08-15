7b50c0c4250163ac2b8b45460895a7f54a926800
Andrii Kovalchuk <coderpy4@proton.me>
2026-08-15 09:57:21 +0300

    refactor(buffering-indicator): add animation when unmounting

--- entrypoints/content/features/player/player-overlay/buffering-indicator.tsx
+++ entrypoints/content/features/player/player-overlay/buffering-indicator.tsx
@@ -1,13 +1,22 @@
+import { AnimatePresence, motion } from 'motion/react';
+
 import { Spinner } from '@/components/ui/spinner';
 
 const BufferingIndicator = () => {
   const { isBuffering } = useIFramePlayer();
-  if (!isBuffering) return null;
 
   return (
-    <div className="pointer-events-none absolute inset-0 z-40 flex h-full w-full items-center justify-center">
-      <Spinner className="size-20" />
-    </div>
+    <AnimatePresence>
+      {isBuffering && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="pointer-events-none absolute inset-0 z-40 flex h-full w-full items-center justify-center">
+          <Spinner className="size-20" />
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
