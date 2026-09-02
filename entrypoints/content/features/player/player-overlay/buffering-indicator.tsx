268b82ad9c2fcd280a4644db2e147ec03d8d17b7
Andrii Kovalchuk <coderpy4@proton.me>
2026-09-03 00:42:46 +0300

    ci: use chrome api v2 for publishing

--- entrypoints/content/features/player/player-overlay/buffering-indicator.tsx
+++ entrypoints/content/features/player/player-overlay/buffering-indicator.tsx
@@ -12,7 +12,8 @@ const BufferingIndicator = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="pointer-events-none absolute inset-0 z-40 flex h-full w-full items-center justify-center">
+          className="pointer-events-none absolute inset-0 z-40 flex h-full w-full items-center justify-center"
+        >
           <Spinner className="size-20" />
         </motion.div>
       )}
